7c451c39f15a52113777722b736a2b19f082eab5
Brandon Scott <scottinnovationgroup@gmail.com>
2026-04-29 17:32:54 -0400

    Added timestamp to PDF output filename

--- src/utils/exportPdf.js
+++ src/utils/exportPdf.js
@@ -3,9 +3,11 @@ import { pdf } from '@react-pdf/renderer'
 import { ResumePdf } from '../components/pdf/ResumePdf'
 
 export async function exportToPdf(data, view, { personName } = {}) {
+  const now = new Date()
+  const stamp = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}-${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}`
   const filename = personName
-    ? `${personName.replace(/\s+/g, '-')}-Resume.pdf`
-    : 'resume.pdf'
+    ? `${personName.replace(/\s+/g, '-')}-Resume-${stamp}.pdf`
+    : `resume-${stamp}.pdf`
 
   const blob = await pdf(createElement(ResumePdf, { data, view })).toBlob()
   const url = URL.createObjectURL(blob)
